ecb8cc434a482b3421a86ae6142fcb7a13c06d69
Boris Abramovich <boris.abramovich@teamaol.com>
2016-03-02 23:27:44 +0200

    Did arrangement, colors

--- app/modules/gg-score/scripts/filters.js
+++ app/modules/gg-score/scripts/filters.js
@@ -1,15 +1,71 @@
 angular.module('score.filters', [])
     .filter(
-    'targetingNameFilter',
-    [
-        'MessageService',
-        function (
-                  messageService) {
+    'teamNameFilter',
+    [function () {
 
-            return function (ts, name) {
-                if (name == '')
-                    return ts;
-                else
-                    return ts.findAll({"name": new RegExp(name,"g")});
+            return function (team) {
+
+                switch(team.toLowerCase()){
+                    case("orlando"):
+                        return "ORL";
+                    case("philadelphia"):
+                        return "PHL";
+                    case("chicago"):
+                        return "CHI";
+                    case("miami"):
+                        return "MIA";
+                    case("phoenix"):
+                        return "PHX";
+                    case("new york"):
+                        return "NY";
+                    case("portland"):
+                        return "POR";
+                    case("boston"):
+                        return "BOS";
+                    case("toronto"):
+                        return "TOR";
+                    case("dallas"):
+                        return "DAL";
+                    case("atlanta"):
+                        return "ATL";
+                    case("washington"):
+                        return "ATL";
+                    case("la lakers"):
+                        return "LAL";
+                    case("golden state"):
+                        return "GSW";
+                    case("san antonio"):
+                        return "SA";
+                    case("milwaukee"):
+                        return "MIL";
+                    case("minnesota"):
+                        return "MIN";
+                    case("denver"):
+                        return "DEN";
+                    case("brooklyn"):
+                        return "BRK";
+                    case("la clippers"):
+                        return "LAC";
+                    case("oklahoma city"):
+                        return "OKC";
+                    case("new orleans"):
+                        return "NO";
+                    case("charlotte"):
+                        return "CHA";
+                    case("houston"):
+                        return "HOU";
+                    case("indiana"):
+                        return "IND";
+                    case("detroit"):
+                        return "DET";
+                    case("sacramento"):
+                        return "SAC";
+                    case("utah"):
+                        return "UTA";
+                    case("memphis"):
+                        return "MEM";
+                    default:
+                        return team;
+                }
             };
         }]);
