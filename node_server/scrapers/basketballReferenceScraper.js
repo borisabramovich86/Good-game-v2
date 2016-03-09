2124fe69e448ddc0d44b3d54a8d69f3a9362a2e3
Boris Abramovich <boris.abramovich@teamaol.com>
2016-03-09 23:06:53 +0200

    Gor more info from Game Stats page

--- node_server/scrapers/basketballReferenceScraper.js
+++ node_server/scrapers/basketballReferenceScraper.js
@@ -27,21 +27,45 @@ function parseUrl(url){
       tidy(html, function(err, html) {
 
         var scoreParserData = scoreParserDataFactory();
+        var mainSelector = 'table.border_gray.x_small_text tr.valign_top > td:nth-child(1) ';
 
         var $ = cheerio.load(html);
-        var ties = $('table.border_gray.x_small_text tr.valign_top > td:nth-child(1) tr:nth-child(2) td:nth-child(2)').text();
-        var leadChanges = $('table.border_gray.x_small_text tr.valign_top > td:nth-child(1) tr:nth-child(3) td:nth-child(2)').text();
-        var gameTied = $('table.border_gray.x_small_text tr.valign_top > td:nth-child(1) tr:nth-child(4) td:nth-child(2)').text();
-        var teamALead = $('table.border_gray.x_small_text tr.valign_top > td:nth-child(1) tr:nth-child(5) td:nth-child(2)').text();
-        var teamBLead = $('table.border_gray.x_small_text tr.valign_top > td:nth-child(1) tr:nth-child(6) td:nth-child(2)').text();
+        var ties = $(mainSelector + 'tr:nth-child(2) td:nth-child(2)').text();
+        var leadChanges = $(mainSelector + 'tr:nth-child(3) td:nth-child(2)').text();
+        var gameTied = $(mainSelector + 'tr:nth-child(4) td:nth-child(2)').text();
+        var teamALead = $(mainSelector + 'tr:nth-child(5) td:nth-child(2)').text();
+        var teamBLead = $(mainSelector + 'tr:nth-child(6) td:nth-child(2)').text();
         console.log('ties: ' + ties);
         console.log('lead changes: ' + leadChanges);
         console.log('game tied: ' + gameTied);
         console.log('team A lead: ' + teamALead);
         console.log('team B lead: ' + teamBLead);
 
-        var b = $('#page_content div.float_left > table:nth-child(3) table:nth-child(1)').first().filter('td').length;
-        console.log(b);
+        var winningTeam = $('#page_content div.float_left > table:nth-child(3) table:nth-child(1)').first().find('td:nth-child(1) a').first().text();
+        var losingTeam = $('#page_content div.float_left > table:nth-child(3) table:nth-child(1)').first().find('td:nth-child(1) a').last().text();
+
+        var quarter = 1;
+        var team = winningTeam;
+        $('#page_content div.float_left > table:nth-child(3) table:nth-child(1)').first().find('td.align_right').each(function(){
+          if($(this).hasClass('bold_text')) {
+            console.log(team + ' Final result ' + $(this).text());
+            quarter = 1;
+            team = losingTeam;
+          }else{
+            console.log(team + ' Quarter '+ quarter + ' ' + $(this).text());
+            quarter++;
+          }
+        });
+
+        var score = '';
+        var pbp = $('#page_content > table td > div:nth-child(3) > table.no_highlight.stats_table').find('td:nth-child(4)').map(function(){
+          if(score !== $(this).text()){
+            score = $(this).text();
+            return $(this).text();
+          }
+        }).get().join(',');
+
+        console.log(pbp);
 
         console.log('Finished parsing ' + url);
         deferred.resolve(collection);
